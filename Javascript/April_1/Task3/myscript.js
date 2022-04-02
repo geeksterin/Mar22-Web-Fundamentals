9f50a5dbf6aec755e6b7341afba5426ac497cece
abhishek <abhishek@gmail.com>
2022-04-03 00:02:15 +0530

    Javascript session notes pushed for 1st and 2nd April

---
+++ Javascript/April_1/Task3/myscript.js
@@ -0,0 +1,16 @@
+
+
+//Javascript is a case sensitive language//
+function myFunction(){
+    document.getElementById('demo').innerHTML='Text has changed';
+    console.log('Test case has passed')
+}
+
+
+//Program to print 10 numbers using for loop//
+
+function print10(){
+    for(let i=0; i<10;i++){
+        console.log(i);
+    }
+}
---
+++ Javascript/April_1/calculator/calculator.js
@@ -0,0 +1,6 @@
+function add() {
+
+    document.getElementById('output').innerHTML = 
+    document.getElementById("firstNumber").value +
+    document.getElementById("secondNumber").value;
+}
