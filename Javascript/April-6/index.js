074eb3b8f20cfd4438154a265efbf928f845c009
Abhishek Ghangas <a.ghangas1@gmail.com>
2022-04-06 22:01:12 +0530

    DOM

---
+++ Javascript/April-6/index.js
@@ -0,0 +1,150 @@
+
+// 1. Take 3 inputs (number ) from the 
+// user and show minimum of the three. 
+
+// 2. Check whether the given character is a vowel or not. 
+// (Also check for capital characters). 
+
+// 3. Write a program to print all even numbers less than or equal to n (n is an input which we take using prompt). 
+// 
+// for( i = 0 to n){
+//     if (i is even){
+//         print i
+//     }
+// }
+
+// 4. Write a program to take an input from the user (number) and check whether it is a prime number or not.
+
+
+// const x = prompt('enter a value for x')
+// const y = prompt('enter a value for y')
+// const numX = parseInt(x)
+// const numY = parseInt(y)
+// // const z = prompt('enter a value for z')
+
+// console.log("value of x,y,z is - ",  x, y )
+
+// if(numX < numY){ 
+//     console.log("x is smaller")
+// }else{
+//     console.log('x is bigger or equal to y')
+// }
+
+// let x = "100"
+// console.log(x+'\n'+x)
+
+// for(let i = 1;i<=100;i++){
+//     console.log(x)
+//     console.log('\n')
+// }
+
+
+// let x = "100"
+
+// console.log(x)
+
+// let y = parseInt(x) // used to convert string to number
+// console.log( y)
+
+// const x = 2
+// x = 3
+
+
+// for(let i = 0;i<=19;i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// const x='first value'
+// x='second number '
+
+// const x = "a"
+
+// if(x === "a"){
+
+// }else if(x === "b"){
+
+// }
+
+// switch(x){
+//     case 'a':
+//         console.log("hi ")
+//         break;
+//     case 'b':
+//         console.log("bye ")
+//     case 'c':
+// }
+
+
+
+
+// function printName(str){
+//     const y = 'hello '+ str
+//     //50 more
+//     console.log(y)
+// }
+
+// printName('GEEK')
+// printName('janejne')
+// printName('ewioJ')
+// printName('MEWKMRNKWM')
+
+// function mulByTwo(num){
+//     // 50 lines of code
+//     return num*2
+// }
+
+// const x = mulByTwo(5)
+// console.log(x)
+
+// function to find largest element of array
+
+// maxnum = 2
+
+
+// const abhishek = [2,4,7,9,4]
+
+// function printMax(someArray){
+//     let maxNum = someArray[0]
+//     for(let i = 1; i<someArray.length;i++){
+//         if(someArray[i] > maxNum){
+//             maxNum = someArray[i]
+//         }
+//     }
+//     console.log(maxNum)
+//     return maxNum
+// }
+
+// printMax(abhishek)
+
+// DOM = document object modal
+// it is a tree structure containing all HTML 
+// elements in their hierarchy
+
+// console.log(document)
+
+// getElementById()
+
+// DOM manupilation
+
+const x = document.getElementById('my-h3')
+const y = document.getElementsByClassName('h3-with-class')
+const z = document.getElementsByTagName('h1')
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+for(let i = 0; i<y.length;i++){
+    y[i].innerText = 'this is new text'
+}
+
+// x.innerText = 'this is new text'
+
+// assignment -
+// 1. Take 3 inputs (number ) from the 
+// user and show minimum of the three. 
+
+// 2. Check whether the given character is a vowel or not. 
+// (Also check for capital characters). 
